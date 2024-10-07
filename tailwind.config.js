45c1e143a0ebb332c49fd145583de52bf2f33231
Alex Besse Donato <alexbessedonato03@gmail.com>
2024-10-07 23:24:10 +0200

    sin purgado

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,8 @@
 module.exports = {
-  content: [
-    "./src/App.tsx", // Archivo principal de tu aplicación
-    "./src/components/**/*.{js,ts,jsx,tsx}", // Todos los archivos en la carpeta components
-  ],
+  content: ["./src/App.tsx", "./src/components/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-  safelist: [
-    "bg-purple-500",
-    "text-white",
-    "bg-gray-200",
-    "text-black",
-    "text-5xl",
-    "text-3xl",
-    "font-bold",
-    "rounded-full",
-    "hover:bg-gray-800",
-  ],
+  purge: false, // Deshabilitar purgado temporalmente
 };
