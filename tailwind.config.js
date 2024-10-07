7334991fc616a807bf6094d4d4da8aa4d46bc6d4
Alex Besse Donato <alexbessedonato03@gmail.com>
2024-10-07 23:17:20 +0200

    tailwind cambios

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,18 @@
 module.exports = {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}", // Asegúrate de incluir todas las extensiones usadas en tu proyecto
+    "./src/**/*.{js,ts,jsx,tsx}", // Verifica las rutas
+  ],
+  safelist: [
+    "bg-purple-500",
+    "text-white",
+    "bg-gray-200",
+    "text-black",
+    "text-5xl",
+    "text-3xl",
+    "font-bold",
+    "rounded-full",
+    "hover:bg-gray-800",
   ],
   theme: {
     extend: {},
